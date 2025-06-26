8a7dbc39675c371194c9ad7f9f03e8b0372450ae
Aleksandar-Mihail <93515589+dankyyyyy@users.noreply.github.com>
2025-06-26 09:11:20 +0200

    Changed language
    Swapped German and French for a more consistent user experience

--- app.py
+++ app.py
@@ -5,7 +5,7 @@ from tkinter import ttk, filedialog, messagebox
 import csv
 import pyperclip
 
-LANGUAGES = ["English", "German", "French", "Danish"]
+LANGUAGES = ["English", "French", "German", "Danish"]
 
 class CSVTabbedLangCopierApp:
     def __init__(self, root):
